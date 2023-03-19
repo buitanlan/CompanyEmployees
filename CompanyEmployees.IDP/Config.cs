c2d63c15c6f40e409fd0d09218de5b5de5e136ba
Bùi Tấn Lân <tanlanhcmus@gmail.com>
2023-03-20 01:13:47 +0700

    initial

---
+++ CompanyEmployees.IDP/Config.cs
@@ -0,0 +1,41 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+
+namespace CompanyEmployees.IDP;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        { 
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile()
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+            { };
+    
+    public static IEnumerable<ApiResource> Apis => 
+        new ApiResource[] 
+            { }; 
+
+    public static IEnumerable<Client> Clients =>
+        new Client[] 
+        { 
+            new()
+            { 
+                ClientName = "CompanyEmployeeClient", 
+                ClientId = "companyemployeeclient", 
+                AllowedGrantTypes = GrantTypes.Code, 
+                RedirectUris = new List<string>{ "https://localhost:5010/signin-oidc" }, 
+                AllowedScopes = { IdentityServerConstants.StandardScopes.OpenId, 
+                    IdentityServerConstants.StandardScopes.Profile }, 
+                ClientSecrets = { new Secret("CompanyEmployeeClientSecret".Sha512()) }, 
+                RequirePkce = true,  
+                RequireConsent = true,
+                PostLogoutRedirectUris = new List<string> { "https://localhost:5010/signout-callback-oidc" } 
+
+            } 
+        };
+}
---
+++ CompanyEmployees.IDP/Pages/TestUsers.cs
@@ -0,0 +1,54 @@
+// Copyright (c) Duende Software. All rights reserved.
+// See LICENSE in the project root for license information.
+
+
+using IdentityModel;
+using System.Security.Claims;
+using System.Text.Json;
+using Duende.IdentityServer;
+using Duende.IdentityServer.Test;
+
+namespace CompanyEmployees.IDP;
+
+public class TestUsers
+{
+    public static List<TestUser> Users
+    {
+        get
+        {
+            var address = new
+            {
+                street_address = "One Hacker Way",
+                locality = "Heidelberg",
+                postal_code = 69118,
+                country = "Germany"
+            };
+                
+            return new List<TestUser>
+            {
+                new()
+                { 
+                    SubjectId = "a9ea0f25-b964-409f-bcce-c923266249b4", 
+                    Username = "John", 
+                    Password = "JohnPassword", 
+                    Claims = new List<Claim> 
+                    { 
+                        new("given_name", "John"), 
+                        new("family_name", "Doe") 
+                    } 
+                }, 
+                new()
+                { 
+                    SubjectId = "c95ddb8c-79ec-488a-a485-fe57a1462340", 
+                    Username = "Jane", 
+                    Password = "JanePassword", 
+                    Claims = new List<Claim> 
+                    { 
+                        new("given_name", "Jane"), 
+                        new("family_name", "Doe") 
+                    }
+                }
+            };
+        }
+    }
+}
