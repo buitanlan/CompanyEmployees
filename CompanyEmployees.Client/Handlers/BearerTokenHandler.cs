702e9ba6f84213cfa91585d3de5d2704c8283909
BÙI TẤN LÂN <tanlanhcmus@gmail.com>
2024-01-05 18:47:57 +0700

    feat(): 07.2 Providing a access token when calling an API

---
+++ CompanyEmployees.Client/Handlers/BearerTokenHandler.cs
@@ -0,0 +1,20 @@
+﻿using IdentityModel.Client;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+
+namespace CompanyEmployees.Client.Handlers;
+
+public class BearerTokenHandler(IHttpContextAccessor httpContextAccessor) : DelegatingHandler
+{
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+        CancellationToken cancellationToken)
+    {
+        var accessToken = await httpContextAccessor.HttpContext.GetTokenAsync(OpenIdConnectParameterNames.AccessToken);
+
+        if (string.IsNullOrWhiteSpace(accessToken))
+            return new HttpResponseMessage(System.Net.HttpStatusCode.Unauthorized);
+
+        request.SetBearerToken(accessToken);
+        return await base.SendAsync(request, cancellationToken);
+    }
+}
--- CompanyEmployees.Client/Program.cs
+++ CompanyEmployees.Client/Program.cs
@@ -1,4 +1,5 @@
 using System.IdentityModel.Tokens.Jwt;
+using CompanyEmployees.Client.Handlers;
 using IdentityModel;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
@@ -16,7 +17,7 @@ builder.Services.AddHttpClient("APIClient", client =>
     client.BaseAddress = new Uri("https://localhost:5001/"); 
     client.DefaultRequestHeaders.Clear(); 
     client.DefaultRequestHeaders.Add(HeaderNames.Accept, "application/json"); 
-});
+}).AddHttpMessageHandler<BearerTokenHandler>();
 
 builder.Services.AddHttpClient("IDPClient", client => 
 { 
@@ -55,6 +56,9 @@ builder.Services.AddAuthentication(opt =>
         RoleClaimType = JwtClaimTypes.Role 
     }; 
 });
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddTransient<BearerTokenHandler>();
 
 var app = builder.Build();
 
