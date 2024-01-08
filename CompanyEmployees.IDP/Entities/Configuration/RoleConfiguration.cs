c26cff89b070d7ff110ca950a48392d84f085b26
Bùi Tấn Lân <tanlanhxmus@gmail.com>
2024-01-09 01:02:24 +0700

    feat(): 10.2 migration role

---
+++ CompanyEmployees.IDP/Entities/Configuration/RoleConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CompanyEmployees.IDP.Entities.Configuration;
+
+public class RoleConfiguration : IEntityTypeConfiguration<IdentityRole>
+{
+    public void Configure(EntityTypeBuilder<IdentityRole> builder)
+    {
+        builder.HasData(
+        [
+            new IdentityRole
+            {
+                Name = "Administrator",
+                NormalizedName = "ADMINISTRATOR",
+                Id = "2c5e174e-3b0e-446f-86af-483d56fd7210",
+            },
+            new IdentityRole
+            {
+                Name = "Visitor",
+                NormalizedName = "VISITOR",
+                Id = "a9ea0f25-b964-409f-bcce-c923266249b4",
+            }
+        ]);
+    }
+}
--- CompanyEmployees.IDP/Entities/UserContext.cs
+++ CompanyEmployees.IDP/Entities/UserContext.cs
@@ -1,6 +1,14 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using CompanyEmployees.IDP.Entities.Configuration;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CompanyEmployees.IDP.Entities;
 
-public class UserContext(DbContextOptions<UserContext> options) : IdentityDbContext<User>(options);
+public class UserContext(DbContextOptions<UserContext> options) : IdentityDbContext<User>(options)
+{
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.ApplyConfiguration(new RoleConfiguration());
+    }
+}
